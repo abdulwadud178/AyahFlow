20dd9db2ce3698dd5d5a9fea12daf558e4eee428
Yousuf-cse <yousuf.mallik79@gmail.com>
2026-04-17 02:08:33 +0530

    fix: fix manifest typos

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,8 @@
-import path from 'path'
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from "path";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 import { VitePWA } from "vite-plugin-pwa";
-
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,31 +12,33 @@ export default defineConfig({
     VitePWA({
       registerType: "autoUpdate",
       devOptions: {
-        enabled: true
+        enabled: true,
       },
       manifest: {
-        name: "My PWA App",
-        short_name: "PWA",
-        description: "My awesome PWA app",
+        name: "Ayah Flow",
+        short_name: "AyahFlow",
+        description: "Ayah Flow PWA",
         theme_color: "#000000",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "/manifest-icon-192.maskable.png",
             sizes: "192x192",
-            type: "image/png"
+            type: "image/png",
+            purpose: "any maskable",
           },
           {
-            src: "/pwa-512x512.png",
+            src: "/manifest-icon-512.maskable.png",
             sizes: "512x512",
-            type: "image/png"
-          }
-        ]
-      }
-    })
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    }),
   ],
-   resolve: {
+  resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-})
+});
