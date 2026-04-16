a0bf73ce1246d861b6cdb8e40f9f5cff9e7f62f2
Yousuf-cse <yousuf.mallik79@gmail.com>
2026-04-17 01:53:30 +0530

    feat: setup for pwa (vite-pwa, workbox, pwa-asset-generator)

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
 import './App.css'
+import { useRegisterSW } from "virtual:pwa-register/react";
+import { Button } from './components/ui/button'
 
 function App() {
 
+const intervalMS = 60 * 60 * 1000;
+
+ const {
+  needRefresh: [needRefresh],
+  offlineReady: [offlineReady],
+  updateServiceWorker
+} = useRegisterSW({
+  onRegistered(registration) {
+    if (registration) {
+      setInterval(() => {
+        registration.update();
+      }, intervalMS);
+    }
+  }
+});
+
+
   return (
     <>
-      <h1 className='bg-green-300 text-center'>AyahFlow</h1>
+      <div>
+      <h1 className='bg-green-300 text-5xl'>Ayah Flow - pwa</h1>
+
+      {offlineReady && <p className='bg-gray-500 text-2xl'>App ready to work offline</p>}
+
+      {needRefresh && (
+        <Button className='bg-amber-600 ' onClick={() => updateServiceWorker(true)}>
+          Update Available — Reload
+        </Button>
+      )}
+    </div>
     </>
   )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import { VitePWA } from "vite-plugin-pwa";
 
 
 // https://vite.dev/config/
@@ -9,6 +10,30 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    VitePWA({
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true
+      },
+      manifest: {
+        name: "My PWA App",
+        short_name: "PWA",
+        description: "My awesome PWA app",
+        theme_color: "#000000",
+        icons: [
+          {
+            src: "/pwa-192x192.png",
+            sizes: "192x192",
+            type: "image/png"
+          },
+          {
+            src: "/pwa-512x512.png",
+            sizes: "512x512",
+            type: "image/png"
+          }
+        ]
+      }
+    })
   ],
    resolve: {
     alias: {
